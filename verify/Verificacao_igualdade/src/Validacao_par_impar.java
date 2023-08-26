68e235b9bdf4cfbd560951f62efcdcc3ba9149ce
Thiago Santana <139005368+SktPeixee@users.noreply.github.com>
2023-08-26 15:52:17 -0300

    Add files via upload
    Código feito em Java, para verificação de Igualdade de dois valores

---
+++ verify/Verificacao_igualdade/src/Validacao_par_impar.java
@@ -0,0 +1,12 @@
+
+public class Validacao_par_impar {
+
+	public static void main(String[] args) {
+		int valorOrigem = 1999;
+		if(valorOrigem % 2 == 0) {
+			System.out.print("Par");
+		} else {
+			System.out.print("Impar");
+		}
+	}
+}
---
+++ verify/Verificacao_igualdade/src/Verify.java
@@ -0,0 +1,13 @@
+
+public class Verify {
+	public static void main(String[] args) {
+		int n1 = 5;
+		int n2 = 5;
+		
+		if(n1 == n2) {
+			System.out.print("Numeros iguais");
+		} else {
+			System.out.print("Numeros diferentes");
+		}
+	}
+}
